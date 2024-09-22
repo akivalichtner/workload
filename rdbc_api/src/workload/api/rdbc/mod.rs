28f9bdc7e6a24c847947882c7801def8bd2c3648
EC2 Default User <ec2-user@ip-172-31-34-73.ec2.internal>
2024-09-22 20:18:40 +0000

    no comment

--- rdbc_api/src/workload/api/rdbc/mod.rs
+++ rdbc_api/src/workload/api/rdbc/mod.rs
@@ -1,12 +1,61 @@
 
+use DatabaseError::NotImplemented;
+
 pub fn connect(_connect_str: &str) -> Result<Connection, DatabaseError> {
-    Result::Err(DatabaseError::ConnectionRefused)
+    Err(NotImplemented)
 }
 
 pub struct Connection {
 
 }
 
+impl Connection {
+
+    pub fn create_statement() -> Result<Statement, DatabaseError> {
+        Err(DatabaseError::NotImplemented)
+    }
+
+    pub fn close() -> Result<(), DatabaseError> {
+        Err(DatabaseError::NotImplemented)
+    }
+}
+
+pub struct Statement {
+
+}
+
+impl Statement {
+
+    pub fn execute_query(_query: &str) -> Result<ResultSet, DatabaseError> {
+        Err(DatabaseError::NotImplemented)
+    }
+
+    pub fn close() -> Result<(), DatabaseError> {
+        Err(DatabaseError::NotImplemented)
+    }
+}
+
+pub struct ResultSet {
+
+}
+
+impl ResultSet {
+
+    pub fn has_next() -> Result<bool, DatabaseError> {
+        Err(DatabaseError::NotImplemented)
+    }
+
+    pub fn next() -> Result<(), DatabaseError> {
+        Err(DatabaseError::NotImplemented)
+    }
+
+    pub fn get_str(_column: u8) -> Result<String, DatabaseError> {
+        Err(DatabaseError::NotImplemented)
+    }
+
+}
+
 pub enum DatabaseError {
-    ConnectionRefused
+    ConnectionRefused,
+    NotImplemented
 }
