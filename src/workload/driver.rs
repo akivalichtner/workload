a453438192227e57cfa2d9eebc20881340d46dd4
EC2 Default User <ec2-user@ip-172-31-45-220.us-east-2.compute.internal>
2024-11-11 15:45:09 +0000

    no comment

--- src/workload/driver.rs
+++ src/workload/driver.rs
@@ -1,70 +1,20 @@
-use std::io::Error;
-use std::net::TcpStream;
-pub struct Driver {}
 
-impl Driver {
-    pub fn connect(host: &str, port: u16) -> Result<Connection, DatabaseError> {
-        let mut connection = Connection {
-            host: String::from(host),
-            port: port,
-            tcp_stream: None,
-            error: None,
-        };
-        connection.connect();
-        if connection.connected() {
-            Ok(connection)
-        } else {
-            Err(DatabaseError::ConnectionFailed)
-        }
-    }
+pub fn new_data_source() -> Box<dyn DataSource> {
+    Box::new(DataSourceImpl{})
 }
 
-pub struct Connection {
-    host: String,
-    port: u16,
-    tcp_stream: Option<TcpStream>,
-    error: Option<Error>,
+pub trait DataSource {
+    
 }
 
-impl Connection {
-    fn connect(&mut self) -> () {
-        let address = format!("{}:{}", &self.host, self.port);
-        match TcpStream::connect(&address) {
-            Ok(tcp_stream) => self.tcp_stream = Some(tcp_stream),
-            Err(error) => self.error = Some(error),
-        }
-    }
+struct DataSourceImpl {
+}
 
-    fn connected(&self) -> bool {
-        match self.tcp_stream {
-            Some(_) => true,
-            None => false
-        }
-    }
-
-    fn create_statement(&mut self) -> Result<Statement, DatabaseError> {
-        Ok(Statement { connection: self })
-    }
-
-    fn close(&mut self) -> Result<(), DatabaseError> {
-        todo!();
-    }
+impl DataSource for DataSourceImpl {
 
 }
 
-pub struct Statement<'a> {
-    connection: &'a Connection
-}
+fn _test() {
+    let _data_source_factory = new_data_source();
 
-impl Statement<'_> {
-
-    fn close(&mut self) -> Result<(), DatabaseError> {
-        todo!();
-    }
-
-}
-
-pub enum DatabaseError {
-    ConnectionFailed,
-    NotImplemented,
 }
